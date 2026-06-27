65c2d2f6859ff94fffcee9d7b6fe26bb0b7bc192
RAHU1027 <sanjitrai88888888@gmail.com>
2026-06-27 12:24:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    # Yahan hum Playwright ka logic add karenge baad mein
+    return f"Login request received for: {username}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
